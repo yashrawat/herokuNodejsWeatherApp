6a4312136c8b768d34db7f019b18f313dcc03fd6
Yash Rawat <yashrawat225@gmail.com>
2020-06-18 10:32:25 +0530

    forecast updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Humidity = ${body.current.humidity}`);
         }
     });
 };
